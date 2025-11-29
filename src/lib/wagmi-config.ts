e98c894cdf3dca8cbfcbc94b25ded7511fe3efde
Afrijal Dzuhri <hi@afrijal.dev>
2025-11-29 13:16:01 +0700

    refactor: simplify chains configuration in wagmi config

--- src/lib/wagmi-config.ts
+++ src/lib/wagmi-config.ts
@@ -5,11 +5,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 export const config = getDefaultConfig({
   appName: "Fe Bootcamp Web3 Jogja App",
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
-  chains: [
-    {
-      ...baseSepolia,
-    },
-  ],
+  chains: [baseSepolia],
   transports: {
     [baseSepolia.id]: http(),
   },
